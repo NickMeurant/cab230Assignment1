489a3eb5320c56fe1343f1886c1fd6a86d4d56b2
NickMeurant <n10485571@qut.edu.au>
2022-04-19 13:28:01 +1000

    table able to call api when form submitted

--- src/routes/VolcanoList.js
+++ src/routes/VolcanoList.js
@@ -6,29 +6,28 @@ import "ag-grid-community/dist/styles/ag-grid.css";
 
 
 export default function VolcanoList() {
+  const distances = [5, 30, 100]
 
   const [countries, setCountries] = useState([]);
-  const [selectedCountry, setSelectedCountry] = useState("Chile");
-  const [distance, setDistance] = useState(5);
+
+  const [selectedCountry, setSelectedCountry] = useState(countries[0]);
+  const [distance, setDistance] = useState(distances[0]);
+
   const [volcanos, setVolcanos] = useState([]);
-  const [rowData, setRowData] = useState([]);
-  const [loaded, setLoaded] = useState(false);
   const gridRef = useRef();
 
-  const distances = [5,30,100]
 
   const [columnDefs] = useState([
     { headerName: "Country", field: 'country' },
-    { headerName: "Name", field: 'name'},
+    { headerName: "Name", field: 'name' },
     { headerName: "Region", field: 'region' },
     { headerName: "SubRegion", field: 'subregion' },
-
   ]);
 
   const GetCountires = async () => {
     const url = "http://sefdb02.qut.edu.au:3001/countries";
     await axios.get(url).then((res) => {
-      console.log(res.data);
+      // console.log(res.data);
       return setCountries(res.data);
     }).catch((error) => {
       console.log(error);
@@ -39,8 +38,8 @@ export default function VolcanoList() {
     const url = "http://sefdb02.qut.edu.au:3001/volcanoes?country=Chile"
       + "&populatedWithin=" + distance + "km";
     await axios.get(url).then((res) => {
-      console.log(res.data);
-      return setRowData(res.data);
+      // console.log(res.data);
+      return setVolcanos(res.data);
     }).catch((error) => {
       console.log(error);
     })
@@ -51,8 +50,16 @@ export default function VolcanoList() {
     GetCountires().then(() => {
       GetVolcanos();
     })
-    setLoaded(true);
   }, [])
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const url = "http://sefdb02.qut.edu.au:3001/volcanoes?country=" + selectedCountry +
+      "&populatedWithin=" + distance+ "km";
+      await axios.get(url).then((res) => {
+        return setVolcanos(res.data);
+      })
+  }
 
   const onFilterTextBoxChanged = useCallback(() => {
     gridRef.current.api.setQuickFilter(
@@ -60,29 +67,32 @@ export default function VolcanoList() {
     );
   }, []);
 
-  
+
   return (
     <div id="myGrid" class="ag-theme-alpine-dark" style={{ height: "600px", width: "80%" }}>
       <div className="example-header">
-        <input
-          type="text"
-          id="filter-text-box"
-          placeholder="Filter..."
-          onInput={onFilterTextBoxChanged}
-        />
-        <label for="country">Select a country</label>
-        <select name="country" id="country">
-        {countries.map((data) =>
-          <option value={data}>{data}</option>)}
-        </select>
-        <label for="distance">Select Distance</label>
-        <select name="distance" id="distance">
-        {distances.map((data)=>
-          <option value={data}>{data}</option>)}
-        </select>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="filter-text-box"
+            placeholder="Filter..."
+            onInput={onFilterTextBoxChanged}
+          />
+          <label for="country">Select a country</label>
+          <select name="country" id="country" onChange={(e) => setSelectedCountry(e.target.value)}>
+            {countries.map((data) =>
+              <option value={data}>{data}</option>)}
+          </select>
+          <label for="distance">Select Distance</label>
+          <select name="distance" id="distance" onChange={(e) => setDistance(e.target.value)}>
+            {distances.map((data) =>
+              <option value={data}>{data}</option>)}
+          </select>
+          <button type="submit">Search</button>
+        </form>
       </div>
       <AgGridReact
-        rowData={rowData}
+        rowData={volcanos}
         columnDefs={columnDefs}
         pagination={true}
         ref={gridRef}
