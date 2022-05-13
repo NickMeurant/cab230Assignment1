46e98d6a889b5ff484863ee87703ee837c1467f7
NickMeurant <n10485571@qut.edu.au>
2022-05-13 17:37:08 +1000

    image on main menu

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import volcano from "../assets/volcano.png";
+import volcano from "../assets/volcano.jpg";
 
 export default function LandingPage(props) {
   const header = props.Header
@@ -11,7 +11,14 @@ export default function LandingPage(props) {
         <div>
         {header}
         </div>
-          <img src={volcano} className="App-logo" alt="logo" />
+        <div className="center-div">
+        <p className="title">
+        Welcome To Your One-Stop Volcano Info Shop
+        </p>
+        </div>
+        <div className="center-div">
+        <img src={volcano} className="App-logo" alt="logo" />
+        </div>
       </div>
     )
 }
