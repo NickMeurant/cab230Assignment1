b67fa3cbeceb9221ef90f84ca801c3678b8dc863
NickMeurant <n10485571@qut.edu.au>
2022-05-20 09:17:53 +1000

    fixed stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 
 export default function Header(props) {
     const navigate = useNavigate();
-    console.log(window.location.pathname);
     return (
         <div>
             <AppBar position= "static">
